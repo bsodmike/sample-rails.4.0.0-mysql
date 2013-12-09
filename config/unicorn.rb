43cce87920bafea258f9d8c83dcd40d8498a5b5c
Michael de Silva <michael@mwdesilva.com>
2013-12-09 21:15:32 +0530

    Config for chef deployment

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,38 +1,46 @@
-worker_processes 2
+# config/unicorn.rb
 
-working_directory "#{ENV['RAILS_STACK_PATH']}"
+rails_env = ENV['RAILS_ENV'] || 'production'
 
-listen "/tmp/web_server.sock", :backlog => 64
-listen 8081, :tcp_nopush => true
-
-timeout 30
-
-pid '/tmp/web_server.pid'
-
-stderr_path "#{ENV['RAILS_STACK_PATH']}/log/unicorn.stderr.log"
-stdout_path "#{ENV['RAILS_STACK_PATH']}/log/unicorn.stdout.log"
+worker_processes (rails_env == 'production' ? 1 : 3)
 
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and
-	GC.copy_on_write_friendly = true
 
-check_client_connection false
+# Restart any workers that haven't responded in 30 seconds
+timeout 30
 
-before_fork do |server, worker|
-	old_pid = '/tmp/web_server.pid.oldbin'
-	if File.exists?(old_pid) && server.pid != old_pid
-		begin
-			Process.kill("QUIT", File.read(old_pid).to_i)
-		rescue Errno::ENOENT, Errno::ESRCH
-			# someone else did our job for us
-		end
-	end	
+working_directory '/opt/apps/demo/current'
 
-	defined?(ActiveRecord::Base) and
-		ActiveRecord::Base.connection.disconnect!
+# Listen on a Unix data socket
+pid '/opt/apps/demo/shared/pids/unicorn.pid'
+listen "/opt/apps/demo/tmp/sockets/demo.sock", :backlog => 2048
+
+stderr_path '/opt/apps/demo/shared/log/unicorn.log'
+stdout_path '/opt/apps/demo/shared/log/unicorn.log'
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = "/opt/apps/demo/current/Gemfile"
 end
 
-after_fork do |server, worker|
-	defined?(ActiveRecord::Base) and
-		ActiveRecord::Base.establish_connection
+before_fork do |server, worker|
+  ##
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+
+  old_pid = '/opt/apps/demo/shared/pids/unicorn.pid.oldbin'
+
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
 end
